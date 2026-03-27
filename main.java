9b092ad0d8e22b8d9cd40c0d10cee17f4a318302
ana <anaclaraaugusta@yahoo.com>
2026-03-26 21:21:32 -0300

    Variáveis
    Conceitos principais

--- main.java
+++ main.java
@@ -1,3 +1,25 @@
+// POO -> Programação Orientada Objeto.
+// É uma linguagem Fortemente tipada.
+// Independente de plataforma(Multiplataforma).
+
+// JVM -> Java Virtual Machine
+// A JVM é responsável por realizar a interpretação 
+// do bytecode e pela execução.
+
+// Uma variável é um espaço na memória onde
+// podemos armazenar valores.
 public class main {
-    
+    public static void main(String[] args) {
+        int minhaIdade = 16;
+        String teste = "Hello Word";
+
+        System.out.println(teste);
+        System.out.println(minhaIdade);
+
+    }
+   
+
+}
+class carro{
+
 }
